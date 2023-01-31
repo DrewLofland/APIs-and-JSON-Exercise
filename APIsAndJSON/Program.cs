3840f47871c7872f914ebe56a13e9fdab204eef1
Drew Lofland <alofland25@gmail.com>
2023-01-31 11:57:19 -0500

    finished exercise 1

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace APIsAndJSON
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+
+namespace APIsAndJSON
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            for (var i = 0; i <= 5; i++)
+            {
+                GetQuotes.KanyeQuote();
+
+                GetQuotes.RonQuote();
+            }
         }
+
     }
+            
 }
